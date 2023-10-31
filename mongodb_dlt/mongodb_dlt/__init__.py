928faf2662b9c332e0b58953aac1319175838a05
Zaeem Athar <zaeemathar94@gmail.com>
2023-10-31 15:05:18 +0100

    Updating definitions

--- mongodb_dlt/mongodb_dlt/__init__.py
+++ mongodb_dlt/mongodb_dlt/__init__.py
@@ -1,11 +1,9 @@
 from dagster import Definitions, load_assets_from_modules, define_asset_job
 
-from .assets import load_mongo_database
+from .assets import dlt_assets, jobs
 
-all_assets = load_assets_from_modules([assets])
-mongo_pipeline = define_asset_job(name="mongo_pipeline", selection= ['load_mongo_database'])
 
 defs = Definitions(
-    assets=all_assets,
-    jobs=[mongo_pipeline],
+    assets=dlt_assets,
+    jobs=jobs,
 )
