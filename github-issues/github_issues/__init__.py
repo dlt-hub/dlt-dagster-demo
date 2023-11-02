c3dd1c583cd292ee9549cd4bbe9c45ab7aefce96
Zaeem Athar <zaeemathar94@gmail.com>
2023-11-02 12:34:03 +0100

    Adding the config params.

--- github-issues/github_issues/__init__.py
+++ github-issues/github_issues/__init__.py
@@ -1,7 +1,7 @@
 from dagster import Definitions, load_assets_from_modules, define_asset_job
 
 from .assets import issues_pipeline
-from .resources import DltPipeline
+from .resources import DltResource
 
 all_assets = load_assets_from_modules([assets])
 simple_pipeline = define_asset_job(name="simple_pipeline", selection= ['issues_pipeline'])
@@ -10,6 +10,11 @@ defs = Definitions(
     assets=all_assets,
     jobs=[simple_pipeline],
     resources={
-        "pipeline": DltPipeline(),
+        "pipeline": DltResource(
+            pipeline_name = "github_issues",
+            dataset_name = "dagster_github_issues",
+            destination = "bigquery",
+            table_name= "github_issues"
+        ),
     }
 )
