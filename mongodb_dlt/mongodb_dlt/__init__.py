878efd7268f984c7a2e50b555f9bfa197b4edad2
Zaeem Athar <zaeemathar94@gmail.com>
2023-11-02 13:29:52 +0100

    Adding the resource definitions.

--- mongodb_dlt/mongodb_dlt/__init__.py
+++ mongodb_dlt/mongodb_dlt/__init__.py
@@ -1,9 +1,15 @@
-from dagster import Definitions, load_assets_from_modules, define_asset_job
+from dagster import Definitions, EnvVar
 
-from .assets import dlt_assets, jobs
-
+from .assets import dlt_assets
+from .resources import DltResource
 
 defs = Definitions(
     assets=dlt_assets,
-    jobs=jobs,
+    resources={
+        "pipeline": DltResource(
+            pipeline_name = "mongo",
+            dataset_name = "dagster_mongo",
+            destination = "bigquery"
+        ),
+    }
 )
